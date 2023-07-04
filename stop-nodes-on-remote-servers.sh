b135e49273a60b220e62f1785e1d0629b8a5e74a
NebojsaHorvat42 <nebojsa.horvat@42computing.tech>
2023-07-04 20:00:53 +0200

    Added function which deletes databases of all nodes in order to start test with clean slate

--- stop-nodes-on-remote-servers.sh
+++ stop-nodes-on-remote-servers.sh
@@ -26,9 +26,34 @@ stop_spring_server_on_remote_machine() {
     ssh $remote_host  'docker exec $(docker ps --filter "ancestor=nhtacc/corda-energy-proxy" --format "{{.ID}}") sh -c "kill 1"'
 }
 
+# Do this in order to rewrite node persitance.mv.db files and remove all data from nodes
+scp_node_persitance_files_to_remote_machine() {
+    local remote_hosts=("$@")    
+    # TODO remove exp folder if it exits
+    remote_user="nebojsa"
+    remote_folder_base="/home/nebojsa/energies/project/${experiment_name}"
+    for remote_host in "${remote_hosts[@]}"
+    do
+        folders=($(find "./${experiment_name}/${remote_host}" -mindepth 1 -maxdepth 1 -type d -not -name "performance_scripts_remote" -exec basename {} \;))
+        # echo "Folders in $remote_host folder in local: ${folders[@]} "
+        for folder in "${folders[@]}"; do
+            echo "Rewriting presistance.mv.db file on host $remote_host in folder $folder"
+            echo "scp local file: ./${experiment_name}/${remote_host}/$folder/persitance.mv.db     to folder: $remote_folder_base/$folder"
+            scp  "./${experiment_name}/${remote_host}/$folder/persistence.mv.db" "./${experiment_name}/${remote_host}/$folder/persistence.trace.db"  "$remote_user@$remote_host:$remote_folder_base/$folder"
+        done
+        # echo "Copying data to " $remote_host
+        # folder="./${experiment_name}/${remote_host}/*"
+
+        # ssh $remote_host "mkdir -p $remote_folder"
+
+        # scp -r $folder "$remote_user@$remote_host:$remote_folder/"
+    done
+}
+
 
 #copy localy created folders to remote hosts
 remote_hosts=("omega" "alfa" "beta" "gama")
-# remote_hosts=( "alfa" )
 stop_nodes_on_remote_machine "${remote_hosts[@]}"
 stop_spring_server_on_remote_machine "omega"
+# Deleti nodes databases
+scp_node_persitance_files_to_remote_machine "${remote_hosts[@]}"

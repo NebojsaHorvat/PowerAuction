d9a7212df134aa61dec5207689d638c557f60bdc
NebojsaHorvat42 <nebojsa.horvat@42computing.tech>
2023-07-20 12:37:50 +0200

    Create bid preperation for test finished. CreateBid test work

--- performance_scripts_remote/create_bid_performance.py
+++ performance_scripts_remote/create_bid_performance.py
@@ -6,25 +6,19 @@ import json
 import sys
 
 # Get the command-line arguments
-if len(sys.argv) != 5:
-    print("Usage: create_bid_performance.py [file_name_memory_sum] [file_name_memory_process] [number_of_tests] [valid_auction_id]")
+if len(sys.argv) != 4:
+    print("Usage: create_bid_performance.py [file_name_memory_sum] [file_name_memory_process] [number_of_tests]")
     sys.exit(1)
 
 file_name_memory = sys.argv[1]
 file_name_process = sys.argv[2]
 number_of_tests= int(sys.argv[3])
-auction_id = sys.argv[4]
+auction_id = None
 
 # Find all java processes. While doing this test I made sure that all java processess which are not Corda are shutdown
 process_filter = filter(lambda p: p.name() == "java", psutil.process_iter())
 processes = list(process_filter)
 
-# Make sure that customer is acitive party on server
-response = requests.post("http://localhost:8085/api/auction/switch-party/customer")
-powerC = False
-
-
-################# Create powerPromise
 headers={
     'Content-type':'application/json', 
     'Accept':'application/json'
@@ -34,6 +28,101 @@ proxies = {
     "http": "",
     "https": "",
 }
+
+# Make sure that customer is active party on server
+response = requests.post("http://localhost:8085/api/auction/switch-party/customer")
+
+
+# Check if there is created Auction and if yes, use it
+response = requests.get("http://localhost:8085/api/auction/list",
+    headers=headers,
+    proxies=proxies
+    )
+print("List of Auctions:\n",response.text)
+parsed_data = json.loads(response.text)
+data_list = parsed_data['data']
+if data_list :
+    auction_id = parsed_data['data'][0]['state']['data']['auctionId']
+    print(auction_id)
+else:
+    # We first need to create PowerPromise which will be used for auction creation
+    # Make sure that prosumer is active party on server
+    response = requests.post("http://localhost:8085/api/auction/switch-party/producer")
+    # Make sure that producer has enough cash to crate power promises
+    data='{"party":"producer","amount":"10000"}'
+    response = requests.post("http://localhost:8085/api/auction/issueCash",
+        data=data,
+        headers=headers,
+        proxies=proxies
+        )
+    response = requests.post("http://localhost:8085/api/auction/switch-party/producer")
+    # Make sure that producer has enough cash to crate power promises (for some reason we need to do this two times)
+    data='{"party":"producer","amount":"10000"}'
+    response = requests.post("http://localhost:8085/api/auction/issueCash",
+        data=data,
+        headers=headers,
+        proxies=proxies
+        )
+    time.sleep(20)
+    # In order to create auction we need to have Power promise
+    response = requests.post("http://localhost:8085/api/auction/switch-party/producer")
+    data='{"powerSuppliedInKW":"101.0","deliveryTime":"17-08-2023 04:52:10 PM", "powerSupplyDurationInMin":"60.0"}'
+    response = requests.post("http://localhost:8085/api/auction/asset/create",
+        data=data,
+        headers=headers,
+        proxies=proxies
+        )
+    data='{"powerSuppliedInKW":"101.0","deliveryTime":"17-08-2023 04:52:10 PM", "powerSupplyDurationInMin":"60.0"}'
+    response = requests.post("http://localhost:8085/api/auction/asset/create",
+        data=data,
+        headers=headers,
+        proxies=proxies
+        )
+
+    print("Create pp response:\n",response.text)
+
+    time.sleep(30)
+
+    response = requests.get("http://localhost:8085/api/auction/asset/list",
+        headers=headers,
+        proxies=proxies
+        )
+    print("List of PowerPromises:\n",response.text) 
+    parsed_data = json.loads(response.text)
+    data_list = parsed_data['data']
+    if data_list :
+        # If powerPromise is created now create Auction
+        asset_id = parsed_data['data'][0]['state']['data']['linearId']['id']
+        print("PowerPromise ID: ",asset_id)
+        data=f'{{"assetId": "{asset_id}","basePrice": "1","deadline": "18-09-2023 01:59:07 PM"}}'
+        response = requests.post("http://localhost:8085/api/auction/create",
+        data=data,
+        headers=headers,
+        proxies=proxies
+        )
+        time.sleep(30)
+        # Check if we created Auction, and if yes, use it
+        response = requests.get("http://localhost:8085/api/auction/list",
+            headers=headers,
+            proxies=proxies
+            )
+        print("List of Auctions:\n",response.text)
+        parsed_data = json.loads(response.text)
+        data_list = parsed_data['data']
+        if data_list :
+            auction_id = parsed_data['data'][0]['state']['data']['auctionId']
+            print(auction_id)
+        else:
+            print("We could not create Auction, aborting script")
+            sys.exit()
+    else:
+        print("We could not create Power promise, aborting script")
+        sys.exit()
+
+
+
+################# Create powerPromise
+
 
 for proc in processes:
     proc.pid,proc.cpu_percent()
--- run-tests.sh
+++ run-tests.sh
@@ -8,6 +8,7 @@ run_non_exec_scripts_on_remote_machine() {
     local remote_hosts=("alfa" "beta" "gama")   
     local file_name_memory_sum=$1
     local file_name_memory_process=$2
+    local save_folder=$3
     remote_user="nebojsa"
     for remote_host in "${remote_hosts[@]}"
     do
@@ -15,7 +16,7 @@ run_non_exec_scripts_on_remote_machine() {
         ssh "$remote_user@$remote_host" << EOF
             cd "\$HOME/energies/project/${experiment_name}/performance_scripts_remote/"
             source bin/activate
-            python3 performance_non_exec.py "${file_name_memory_sum}_${remote_host}" "${file_name_memory_process}_${remote_host}"
+            python3 performance_non_exec.py "${save_folder}${file_name_memory_sum}_${remote_host}" "${save_folder}${file_name_memory_process}_${remote_host}"
 EOF
     done
 }
@@ -31,7 +32,7 @@ run_create_PP_scripts_on_remote_machine() {
     ssh "$remote_user@$remote_host" << EOF
         cd "\$HOME/energies/project/${experiment_name}/performance_scripts_remote/"
         source bin/activate
-        python3 create_power_promise_performance.py "${file_name_memory_sum}_${remote_host}" "${file_name_memory_process}_${remote_host}" ${number_of_operations}
+        python3 create_power_promise_performance.py "createPP/${file_name_memory_sum}_${remote_host}" "createPP/${file_name_memory_process}_${remote_host}" ${number_of_operations}
 EOF
 }
 
@@ -46,7 +47,7 @@ run_create_auction_scripts_on_remote_machine() {
     ssh "$remote_user@$remote_host" << EOF
         cd "\$HOME/energies/project/${experiment_name}/performance_scripts_remote/"
         source bin/activate
-        python3 create_auction_performance.py "${file_name_memory_sum}_${remote_host}" "${file_name_memory_process}_${remote_host}" ${number_of_operations} 
+        python3 create_auction_performance.py "createAuction/${file_name_memory_sum}_${remote_host}" "createAuction/${file_name_memory_process}_${remote_host}" ${number_of_operations} 
 EOF
 }
 
@@ -55,14 +56,13 @@ run_create_bid_scripts_on_remote_machine() {
     local file_name_memory_sum=$1
     local file_name_memory_process=$2
     local number_of_operations=$3
-    local auction_id=$4
     remote_user="nebojsa"
 
     echo "SSH connection to remote host: " $remote_host
     ssh "$remote_user@$remote_host" << EOF
         cd "\$HOME/energies/project/${experiment_name}/performance_scripts_remote/"
         source bin/activate
-        python3 create_bid_performance.py "${file_name_memory_sum}_${remote_host}" "${file_name_memory_process}_${remote_host}" ${number_of_operations} ${auction_id}
+        python3 create_bid_performance.py "createBid/${file_name_memory_sum}_${remote_host}" "createBid/${file_name_memory_process}_${remote_host}" ${number_of_operations}
 EOF
 }
 
@@ -77,12 +77,6 @@ run_issue_cache_to_producer_producer() {
 EOF
 }
 
-# test_numbers=(1 10 100 200)
-
-
-
-# In order to crete prower promises we need to have sufficent amount of cash
-test_numbers=(1 10)
 # Create list which will conteaind all 
 tries=()
 for (( i = 1; i <= repetition_number; i++ )); do
@@ -94,21 +88,51 @@ done
 
 #################################### Crete power promise tests
 
+# In order to crete prower promises we need to have sufficent amount of cash
+test_numbers=(1 10 100)
+
+for number_of_try in "${tries[@]}"
+do   
+    file_name_memory_base="memory_${experiment_name}_try${number_of_try}_transactions"
+    file_name_process_base="process_${experiment_name}_try${number_of_try}_transactions"
+    save_folder="createPP/"  
+
+    run_issue_cache_to_producer_producer
+    sleep 10s
+    # Run create PP tests
+    for number_of_tests in "${test_numbers[@]}"
+    do
+        file_name_memory="${file_name_memory_base}_cretePP_${number_of_tests}"
+        file_name_process="${file_name_process_base}_cretePP_${number_of_tests}"
+        run_create_PP_scripts_on_remote_machine "${file_name_memory}" "${file_name_process}" "${number_of_tests}"
+        run_non_exec_scripts_on_remote_machine "${file_name_memory}" "${file_name_process}" "${save_folder}"
+    done
+    
+    # Restart network before next bach of tests
+    ./stop-nodes-on-remote-servers.sh "auto-exp-0"
+    ./run-nodes-on-remote-servers.sh auto-exp-0
+done
+
+#################################### Crete auction tests
+
+
+# test_numbers=(1 10)
 
 # for number_of_try in "${tries[@]}"
 # do   
 #     file_name_memory_base="memory_${experiment_name}_try${number_of_try}_transactions"
 #     file_name_process_base="process_${experiment_name}_try${number_of_try}_transactions"
-
+#     save_folder="createAuction/"
 #     run_issue_cache_to_producer_producer
 #     sleep 10s
-#     # Run create PP tests
+#     # Run create Auction tests
 #     for number_of_tests in "${test_numbers[@]}"
 #     do
-#         file_name_memory="${file_name_memory_base}_cretePP_${number_of_tests}"
-#         file_name_process="${file_name_process_base}_cretePP_${number_of_tests}"
-#         run_create_PP_scripts_on_remote_machine "${file_name_memory}" "${file_name_process}" "${number_of_tests}"
-#         run_non_exec_scripts_on_remote_machine "${file_name_memory}" "${file_name_process}"
+#         echo "RUNIGN TESTS"
+#         file_name_memory="${file_name_memory_base}_creteAuction_${number_of_tests}"
+#         file_name_process="${file_name_process_base}_creteAuction_${number_of_tests}"
+#         run_create_auction_scripts_on_remote_machine "${file_name_memory}" "${file_name_process}" "${number_of_tests}"
+#         run_non_exec_scripts_on_remote_machine "${file_name_memory}" "${file_name_process}" "${save_folder}"
 #     done
     
 #     # Restart network before next bach of tests
@@ -116,31 +140,28 @@ done
 #     # ./run-nodes-on-remote-servers.sh auto-exp-0
 # done
 
-#################################### Crete auction tests
+#################################### Crete Bid tests
 
+# test_numbers=(1 10 100)
 
-test_numbers=(1 10)
-
-for number_of_try in "${tries[@]}"
-do   
-    file_name_memory_base="memory_${experiment_name}_try${number_of_try}_transactions"
-    file_name_process_base="process_${experiment_name}_try${number_of_try}_transactions"
-
-    run_issue_cache_to_producer_producer
-    sleep 10s
-    # Run create Auction tests
-    for number_of_tests in "${test_numbers[@]}"
-    do
-        echo "RUNIGN TESTS"
-        file_name_memory="${file_name_memory_base}_creteAuction_${number_of_tests}"
-        file_name_process="${file_name_process_base}_creteAuction_${number_of_tests}"
-        run_create_auction_scripts_on_remote_machine "${file_name_memory}" "${file_name_process}" "${number_of_tests}"
-        run_non_exec_scripts_on_remote_machine "${file_name_memory}" "${file_name_process}"
-    done
+# for number_of_try in "${tries[@]}"
+# do   
+#     file_name_memory_base="memory_${experiment_name}_try${number_of_try}_transactions"
+#     file_name_process_base="process_${experiment_name}_try${number_of_try}_transactions"
+#     save_folder="createBid/"
+#     run_issue_cache_to_producer_producer
+#     sleep 10s
+#     # Run create Auction tests
+#     for number_of_tests in "${test_numbers[@]}"
+#     do
+#         echo "RUNIGN TESTS"
+#         file_name_memory="${file_name_memory_base}_creteBid_${number_of_tests}"
+#         file_name_process="${file_name_process_base}_creteBid_${number_of_tests}"
+#         run_create_bid_scripts_on_remote_machine "${file_name_memory}" "${file_name_process}" "${number_of_tests}"
+#         run_non_exec_scripts_on_remote_machine "${file_name_memory}" "${file_name_process}" "${save_folder}"
+#     done
     
-    # Restart network before next bach of tests
-    # ./stop-nodes-on-remote-servers.sh "auto-exp-0"
-    # ./run-nodes-on-remote-servers.sh auto-exp-0
-done
-
-
+#     # Restart network before next bach of tests
+#     # ./stop-nodes-on-remote-servers.sh "auto-exp-0"
+#     # ./run-nodes-on-remote-servers.sh auto-exp-0
+# done
